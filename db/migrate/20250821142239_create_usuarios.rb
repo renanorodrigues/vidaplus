36bb2f268439d2edcb9e41891456c85777543a56
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2025-08-21 11:31:04 -0300

    Adiciona migration de Usuario

---
+++ db/migrate/20250821142239_create_usuarios.rb
@@ -0,0 +1,16 @@
+class CreateUsuarios < ActiveRecord::Migration[8.0]
+  def change
+    create_table :usuarios do |t|
+      t.string :primeiro_nome
+      t.string :ultimo_nome
+      t.string :sexo
+      t.string :idade
+      t.json :endereco
+      t.integer :papel
+      t.string :email
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end
