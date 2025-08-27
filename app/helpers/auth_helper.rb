a5e389382bed6d70dd5b5cf168b13b0b900aee15
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2025-08-27 19:38:02 -0300

    Adiciona helper para criar token temporário

---
+++ app/helpers/auth_helper.rb
@@ -0,0 +1,17 @@
+module AuthHelper
+  require 'jwt'
+  TOKEN_EXPIRATION_TIME = 15.minutes.from_now
+
+  def encode_token(user)
+    token = JWT.encode(build_encode(user), ENV['JWT_SECRET'], 'HS256')
+
+    { token: token, exp: TOKEN_EXPIRATION_TIME }
+  end
+
+  def build_encode(user)
+    {
+      user_id: user.id,
+      exp: TOKEN_EXPIRATION_TIME.to_i
+    }
+  end
+end
