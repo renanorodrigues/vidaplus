67988aba2213a57612d184d182c0b03fcb80c718
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2025-08-27 19:36:10 -0300

    Adiciona validações no model Usuario

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,4 +1,14 @@
 class Usuario < ApplicationRecord
   has_secure_password
+  validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}, presence: true, uniqueness: true
+  validates :primeiro_nome, presence: true, length: { maximum: 50 }
   validates :password, presence: true, length: { minimum: 6 }
+
+  before_save :downcase_email
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
 end
