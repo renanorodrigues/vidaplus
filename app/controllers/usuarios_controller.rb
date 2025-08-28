916218c30bf43411c26e9bd76577fecf227d10a0
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2025-08-28 18:34:16 -0300

    Criação de endpoint show no controller UsuariosController

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -1,3 +1,13 @@
 class UsuariosController < ApplicationController
-  before_action :autentica_usuario
+  before_action :authenticate_user
+
+  def show
+    render json: { user: find_user_data }, status: :ok
+  end
+
+  private
+
+  def find_user_data
+    params[:session_user].slice(:name, :email, :created_at)
+  end
 end
