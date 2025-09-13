e3de3964c33dda9d17f40cc6916a1022d00a3960
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2025-09-13 19:13:37 -0300

    Reordena chave extrangeira em Paciente

--- db/migrate/20250828204109_create_pacientes.rb
+++ db/migrate/20250828204109_create_pacientes.rb
@@ -1,12 +1,12 @@
 class CreatePacientes < ActiveRecord::Migration[8.0]
   def change
     create_table :pacientes do |t|
-      t.references :usuario, null: false, foreign_key: true
       t.string :rg
       t.string :cpf
       t.string :contato
       t.string :contato_emergencia
       t.text :observacao
+      t.references :usuario, null: false, foreign_key: true
 
       t.timestamps
     end
