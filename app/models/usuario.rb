c90a0923a5593fcda1dd47ce1e119da4a1d46e05
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2025-09-23 19:41:51 -0300

    Adiciona relacionamentos no model Usuario

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,5 +1,9 @@
 class Usuario < ApplicationRecord
+  has_one :administrador
+  has_one :profissional
+  has_one :paciente
   has_secure_password
+
   validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
   enum :estado, { ativo: 0, desativado:1 }
