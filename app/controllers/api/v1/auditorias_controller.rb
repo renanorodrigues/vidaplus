37f02a1ea5916b4663a25a1bc12ebd8239c2533c
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2025-10-03 20:17:09 -0300

    Renomeia rota e controller de Auditoria

--- app/controllers/api/v1/auditorias_controller.rb
+++ app/controllers/api/v1/auditorias_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class AuditsController < AccessPermissionsController
+    class AuditoriasController < AccessPermissionsController
       before_action :autentica_admin
 
       def index
