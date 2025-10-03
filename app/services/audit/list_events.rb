d813908a29345d573bc82d51daba7f89ebb64c09
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2025-10-03 20:17:50 -0300

    Classe inicial que vai buscar os dados de auditoria de todo o sistema

--- app/services/audit/list_events.rb
+++ app/services/audit/list_events.rb
@@ -2,11 +2,11 @@ module Audit
   class ListEvents
     def initialize(params)
       @params = params
+      @service = PaperTrail::Version
     end
 
     def call
-      # Retorna a lista de eventos via PaperTrail
-      # Pode adicionar filtros aqui se necessário
+      @service.order(created_at: :desc)
     end
   end
 end
