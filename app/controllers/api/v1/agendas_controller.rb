08e6e3839d3e011876fdc01d0e059bc688adc5d6
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2025-10-17 20:36:06 -0300

    Melhoria no controller Agendas

--- app/controllers/api/v1/agendas_controller.rb
+++ app/controllers/api/v1/agendas_controller.rb
@@ -2,28 +2,31 @@ module Api
   module V1
     class AgendasController < AccessPermissionsController
       before_action :autentica_profissional
+      before_action :find_profissional, only: %i[index create]
       before_action :find_agenda, only: %i[show update destroy]
 
       def index
-        @agendas = Agenda.all
-
+        @agendas = @profissional.agendas
         render json: @agendas, status: :ok
       end
 
       def create
-        @agenda = Agenda.new agenda_params
+        agenda = @profissional.agendas.new(agenda_params)
 
-        if @agenda.valid?
-          @agenda.save
-          render json: @agenda, status: :created
+        if agenda.valid?
+          agenda.save
+          render json: agenda, status: :created
         else
-          render json: @agenda.errors, status: :unprocessable_content
+          render json: agenda.errors, status: :unprocessable_content
         end
       end
 
-      def show; end
+      def show
+        render json: @agenda, status: :ok
+      end
 
       def update
+        validate_agenda_by_profissional
         if @agenda.update(agenda_params)
           render json: @agenda, status: :ok
         else
@@ -32,17 +35,40 @@ module Api
       end
 
       def destroy
+        validate_agenda_by_profissional
         @agenda.destroy
       end
 
       private
+
+      def validate_agenda_by_profissional
+        unless @agenda.profissional.usuario_id == current_user.id
+          render json: { error: 'Acesso negado' }, status: :forbidden
+        end
+      end
+
+      def find_profissional
+        @profissional = Profissional.find params[:profissional_id]
+      end
 
       def find_agenda
         @agenda = Agenda.find params[:id]
       end
 
       def agenda_params
-        params.require(:agenda).permit(:data_inicio, :data_termino, :observacao, :estado, :profissional_id, :unidade_medica_id, horarios: [dias_da_semana: [], horarios: [], duracao_agenda_minutos: []])
+        params.require(:agenda).permit(
+          :data_inicio,
+          :data_termino,
+          :observacao,
+          :estado,
+          :profissional_id,
+          :unidade_medica_id,
+          horarios: [
+            dias_da_semana: [],
+            horarios: [],
+            duracao_agenda_minutos: []
+          ]
+        )
       end
     end
   end
